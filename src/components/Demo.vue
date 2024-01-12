b7425e0f8c654219a6325839e795afd08092f1f1
Will Myers <will@otis.solutions>
2024-01-12 15:18:15 -0500

    Using Dev Scripts

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -2,8 +2,18 @@
 import { computed, onMounted, watch } from 'vue';
 import demo from '../demoPage.js';
 
-//const proxyUrl = 'http://localhost:3000';
 const proxyUrl = 'https://proxy.will-myers.com'
+let pluginScriptUrl;
+let pluginStylesUrl;
+if (window.location.host.includes('localhost')) {
+    pluginScriptUrl = window.location.protocol + '//' + window.location.host + '/plugin/index.js'; // Update this to the correct path
+    pluginStylesUrl = window.location.protocol + '//' + window.location.host + '/plugin/styles.css'; // Update this to the correct path
+} else {
+    pluginScriptUrl = 'https://cdn.jsdelivr.net/gh/willmyerscode/nested-folders@1/nested-folders.min.js?t=' + new Date().getTime();
+    pluginStylesUrl = 'https://cdn.jsdelivr.net/gh/willmyerscode/nested-folders@1/nested-folders.min.css?t=' + new Date().getTime();
+}
+
+
 
 
 const props = defineProps({
@@ -43,33 +53,35 @@ Object.entries(props.fields).forEach(([groupKey, group]) => {
             }
             if (field.customProperty && newValue !== oldValue) {
                 const details = stylesWithChanges.value;
-                const message = { 
-                    type: 'addStylesheet', 
+                const message = {
+                    type: 'addStylesheet',
                     id: 'builder',
-                    details:  details,
+                    details: details,
                 };
                 sendMessage(message);
             }
         });
     });
 });
-   
-const onLoadHandler = () => {
-    const details = `
-    
-    `;
-    const message = { 
-        type: 'executeScript', 
-        id: 'builder',
-        details:  details,
-    };
-    sendMessage(message);
-}
 
+const onLoadHandler = async () => {
+    const styleMessage = {
+        type: 'addLink',
+        id: 'plugin-styles',
+        href: pluginStylesUrl
+    };
+    const scriptMessage = {
+        type: 'addScript',
+        id: 'plugin-script',
+        src:  pluginScriptUrl
+    };
+    sendMessage(styleMessage)
+    sendMessage(scriptMessage)
+}
 </script>
 
 <template>
-    <iframe :src="url" @load="onLoadHandler"></iframe>
+    <iframe :src="url" @load="onLoadHandler" allow-scripts></iframe>
 </template>
 
 <style scoped>
