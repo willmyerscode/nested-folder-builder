08734bc3933c1c3bea456bbebbee44c8d8d3a385
Will Myers <will@otis.solutions>
2024-01-15 15:14:24 -0500

    Updated default border styles

--- src/pluginUrls.js
+++ src/pluginUrls.js
@@ -1,6 +1,7 @@
-//const demoPage = 'https://woof.will-myers.com/nested-folders-customizer';
 const pluginUrls = {
     demoPage: 'https://woof.will-myers.com/nested-folders-customizer',
+    livePluginStyles: 'https://cdn.jsdelivr.net/gh/willmyerscode/nested-folders@1/nested-folders.min.css',
+    livePluginScript: 'https://cdn.jsdelivr.net/gh/willmyerscode/nested-folders@1/nested-folders.min.js',
     pluginStyles: 'https://willmyerscode.github.io/nested-folders/nested-folders.css',
     pluginScript: 'https://willmyerscode.github.io/nested-folders/nested-folders.js',
 };
